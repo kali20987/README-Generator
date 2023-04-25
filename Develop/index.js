7e595570da5a232007f19d474520cbc68b2ee8a0
kali19 <khadijah.syed.ali@gmail.com>
2023-04-25 18:52:19 -0400

    index.js review

--- Develop/index.js
+++ Develop/index.js
@@ -1,14 +1,90 @@
-// TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const fs = require('fs');
+
+const generateREADME = ({ name, email, project, description, license, install, tests, uses, contrib }) =>
+  `# ${project} 
+  ## Description ${description}
+## Table of Contents
+* Installation
+* Usage
+* License
+* Tests
+* Questions
+
+## Installation
+${install}
+
+## Usage
+${uses}
+
+## License
+${license}
+
+## Contribution
+${contrib}
+
+## Command to run tests
+${tests}
+
+### Made by ${name}
+### Email: ${email}
+  
+  
+  `;
 
 
-// TODO: Create an array of questions for user input
-const questions = [];
+  inquirer
+    .prompt([
+        {
+            type: 'input',
+            name: 'name',
+            message: 'What is your GitHub username?',
+          },
+          {
+            type: 'input',
+            name: 'email',
+            message: 'What is your email address?',
+          },
+          {
+            type: 'input',
+            name: 'project',
+            message: 'What is your project name?',
+          },
+          {
+            type: 'input',
+            name: 'description',
+            message: 'Please write a short description of your project',
+          },
+          {
+            type: 'input',
+            name: 'license',
+            message: 'What kind of license should your project have?',
+          },
+          {
+            type: 'input',
+            name: 'install',
+            message: 'What command should be run to install dependencies?',
+          },
+          {
+            type: 'input',
+            name: 'tests',
+            message: 'What command should be run to run tests?',
+          },
+          {
+            type: 'input',
+            name: 'uses',
+            message: 'What does the user need to know about using the repo?',
+          },
+          {
+            type: 'input',
+            name: 'contrib',
+            message: 'What does the user need to know about contributing to the repo?',
+          },
+    ])
+    .then((answers) => {
+        const READMEpageContent = generateREADME(answers);
 
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
-
-// TODO: Create a function to initialize app
-function init() {}
-
-// Function call to initialize app
-init();
+        fs.writeFile('README.md', READMEpageContent, (err) => 
+            err ? console.log(err) : console.log('Successfully created README.md!')
+        );
+    });
